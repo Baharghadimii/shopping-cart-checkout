2a28f551661f4e51697aa74c0f9e39b19e2b0a82
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-13 14:52:52 -0800

    itemdetails more details added

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Button, Collapse, Well, Media, Row, Col } from 'react-bootstrap';
+import { Button, Collapse, Media, Row, Col } from 'react-bootstrap';
 
-export default function ItemDetails() {
+export default function ItemDetails(props) {
   const [state, setState] = useState({
     open: false
   })
@@ -14,6 +14,31 @@ export default function ItemDetails() {
         {state.open === false ? 'See' : 'Hide'} item Details
         {state.open === false ? ' +' : ' -'}
       </Button>
+      <Collapse in={state.open}>
+        <div>
+          <Media>
+            <Media.Left>
+              <img
+                width={100}
+                height={100}
+                alt='thumbnail'
+                src="">
+              </img>
+            </Media.Left>
+            <Media.Body>
+              <p>Essentials by OFM ESS-3085 Racing Style Leather Gaming Chair, Red</p>
+              <Row className="show-grid">
+                <Col md={6}>
+                  <strong>{`$${props.price}`}</strong>
+                  <br />
+                  <strong className='price-strike'>{`$${props.price}`}</strong>
+                </Col>
+                <Col md={6}> Qty: 1</Col>
+              </Row>
+            </Media.Body>
+          </Media>
+        </div>
+      </Collapse>
     </div >
   )
 }
