03e85996c3d7b79c417ab93f3ca843987408a1b9
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-13 13:42:59 -0800

    subtotal component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.scss';
+import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
+import Subtotal from './components/Subtotal/Subtotal';
+import './App.scss';
+
 
 function App() {
+
+  const [state, setstate] = useState({
+    total: 100
+  });
   return (
     <div className="container">
       <Container id="purchase-card">
-        <h1>Hello world!</h1>
+        <Subtotal price={state.total.toFixed(2)} />
       </Container>
 
     </div>
