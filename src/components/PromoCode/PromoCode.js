b28d7da04a2ed15f88d17b61f2140cac3f1bd08e
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-13 16:58:37 -0800

    promo code component style changed

--- src/components/PromoCode/PromoCode.js
+++ src/components/PromoCode/PromoCode.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { Button, Collapse, Form, Row, Col, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
-
+import { Button, Collapse, Form, Row, Col, Jumbotron, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+import './PromoCode.scss';
 export default function PromoCode(props) {
   const [state, setState] = useState({
     open: false,
     value: ''
   })
+  const handleChange = () => {
+
+  }
   return (
     <div>
       <Button
@@ -15,7 +18,33 @@ export default function PromoCode(props) {
         {state.open === false ? 'Apply ' : 'Hide '}promo code
         {state.open === false ? " +" : ' -'}
       </Button>
-
+      <Collapse in={state.open}>
+        <div className="div">
+          {/* <Jumbotron > */}
+          <Row className="show-grid">
+            <Form className="form">
+              <FormGroup controlId="formInlineName">
+                <Form.Label style={{ fontWeight: '700' }}>Promo Code</Form.Label>
+                <FormControl
+                  type="text"
+                  placeholder='Enter Promo Code'
+                  value={props.promoCode}
+                  onChange={handleChange}>
+                </FormControl>
+                <Button
+                  block
+                  variant='success'
+                  className='btn-round btn-f'
+                  disabled={props.isDisabled}
+                  onClick={props.giveDiscount}>
+                  Apply
+                  </Button>
+              </FormGroup>
+            </Form>
+          </Row>
+          {/* </Jumbotron> */}
+        </div>
+      </Collapse>
     </div>
   )
 }
