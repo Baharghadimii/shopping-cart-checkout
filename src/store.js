841166567522a91ec43ce652b7c7df49a8ba1436
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-13 17:56:20 -0800

    make store and export id

--- src/store.js
+++ src/store.js
@@ -1,2 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {}
+const middleWare = [thunk]
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleWare),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
+
+export default store;
